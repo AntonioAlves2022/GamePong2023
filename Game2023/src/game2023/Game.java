2762eabb25ad897daefbf253cb48038a3e6d8853
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2023-03-17 14:38:49 -0300

    Criando a tela inicial do game

--- Game2023/src/game2023/Game.java
+++ Game2023/src/game2023/Game.java
@@ -2,6 +2,8 @@ package game2023;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 
@@ -27,11 +29,23 @@ public class Game extends Canvas implements Runnable {
         frame.add(game);
         frame.pack();
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);    
+        frame.setVisible(true); 
+        
+        new Thread(game).start();
     }
     
     void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
         
+        Graphics g = layer.getGraphics();
+        // player.render(g);
+        g = bs.getDrawGraphics();
+        g.drawImage(layer, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+        bs.show();
     }
     
     void tick(){
